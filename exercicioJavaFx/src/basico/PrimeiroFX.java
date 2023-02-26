dd0dfc42f040ba9b7d29e9ad0f95954387eda7c3
felipe <felipesilva4127@gmail.com>
2023-02-26 16:19:51 -0300

    javaFX2

--- exercicioJavaFx/src/basico/PrimeiroFX.java
+++ exercicioJavaFx/src/basico/PrimeiroFX.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class PrimeiroFX extends Application{
@@ -17,14 +17,18 @@ public class PrimeiroFX extends Application{
 		Button botaoB = new Button("B");
 		Button botaoC = new Button("C");
 		
-		VBox box = new VBox();
+		botaoA.setOnAction(e-> System.out.println("A"));
+		botaoA.setOnAction(e-> System.out.println("B"));
+		botaoA.setOnAction(e-> System.exit(0));
+		
+		HBox box = new HBox();
 		box.setAlignment(Pos.CENTER);
 		box.setSpacing(10);
 		box.getChildren().add(botaoA);
 		box.getChildren().add(botaoB);
 		box.getChildren().add(botaoC);
 		
-		Scene cena = new Scene(box,200, 400);
+		Scene cena = new Scene(box,150, 100);
 		
 		primaryStage.setScene(cena);
 		primaryStage.show();
